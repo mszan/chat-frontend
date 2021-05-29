103d426ca54911973c0de5f944c87bda9fc8705e
Dawid Mszanowski <dmszanowski@icloud.com>
2021-05-29 19:58:56 +0200

    initial changes

---
+++ __craco.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//     plugins: [
+//         {
+//             options: {
+//                 customizeTheme: {
+//                     "@primary-color": "#FF5A5F",
+//                     "@layout-header-background": "#484848",
+//                 },
+//             },
+//         },
+//     ],
+// };
